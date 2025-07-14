0984570138348b7aef94404bc2011e96a1af01c9
Tomáš Bednár <bednar.tomas.beda@gmail.com>
2025-07-14 15:05:26 +0200

    fix: mapper

--- Mapper.cs
+++ Mapper.cs
@@ -6,4 +6,15 @@ namespace MapperlyNestedMapping;
 public partial class ClaimedRewardsMappers
 {
     public partial Target Map(Source output);
+
+    [MapperIgnoreSource(nameof(source.Actions))]
+    [MapPropertyFromSource(nameof(Target.RewardItem.Complete), Use = nameof(MapComplete))]
+    private partial Target.RewardItem Map(Source.RewardItem source);
+
+    private static Target.CompleteActionUrl MapComplete(Source.RewardItem sourceItem)
+        => new()
+        {
+            Method = sourceItem.Actions?.Complete?.Method ?? string.Empty,
+            Url = sourceItem.Actions?.Complete?.Url ?? string.Empty
+        };
 }
